c11eafd9639eea272f2505d031c295830c16c9b4
rodrigo brinate <rbp20199@gmail.com>
2022-05-17 22:07:43 -0300

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser= require('body-parser')
 var cors = require('cors')
-
+const db = client.db('acesse')
 var porta = process.env.PORT || 3001;
 
 
@@ -20,7 +20,7 @@ MongoClient.connect(connectionString,  (err, client) => {
     app.get('/',  async function  (req, res) {
         const db = client.db('acesse')
         const quotesCollection = await db.collection('coffee').find().toArray()
-        console.log(quotesCollection)
+      //  console.log(quotesCollection)
         res.json(quotesCollection)
       })
       app.post('/add',  async function  (req, res) {
@@ -28,7 +28,7 @@ MongoClient.connect(connectionString,  (err, client) => {
         var a = await (await db.collection('coffee').find().toArray())
         //console.log(a)
         var b = await (db.collection('coffee').find({ colaborador:req.body.colaborador  }).toArray())
-          console.log(b.length)
+          //console.log(b.length)
         if ( b.length  >= 1){
           res.json({st: 0, msg: "o colaborador já está no café"})
           }else{
@@ -41,6 +41,28 @@ MongoClient.connect(connectionString,  (err, client) => {
        }
          }
       }) 
+
+      app.post('/add-historic',  async function  (req, res) {
+        const db = client.db('acesse')
+        var a = await (await db.collection('coffee-historic').findOne({colaborador:req.body.colaborador, day: new  Date().getDate(), month: new  Date().getMonth(), year: new  Date().getFullYear()}))
+        if (a){
+         await db.collection('coffee-historic').update({colaborador:req.body.colaborador, day: new  Date().getDate(), month: new  Date().getMonth(), year: new  Date().getFullYear()}, { $set: { minutos: a.minutos + req.body.minutos }});
+        console.log(a)}else {
+          console.log(a)
+        await db.collection('coffee-historic').insertOne({colaborador: req.body.colaborador,minutos: req.body.minutos, day: new  Date().getDate(), month: new Date().getMonth(), year: new Date().getFullYear()})
+       }
+       res.json({st: 1, msg: "colaborador terminou o horário de café"})
+         
+      })
+
+      app.get('/historic',  async function  (req, res) {
+        
+        const quotesCollection = await db.collection('coffee-historic').find({day: new  Date().getDate(), month: new  Date().getMonth(), year: new  Date().getFullYear()}).toArray()
+      //  console.log(quotesCollection)
+        res.json(quotesCollection)
+      })
+
+
       app.post('/remove',  async function  (req, res) {
         const db = client.db('acesse')
         const quotesCollection = await db.collection('coffee').deleteOne({colaborador: req.body.colaborador})
